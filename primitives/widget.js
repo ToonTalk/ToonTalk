55aaf42deac1e8f09dd4177ef0d3d54e17533dd6
ToonTalk <toontalk@gmail.com>
2014-02-07 13:47:11 +0000

    Added error reporting for undefined widget methods

--- primitives/widget.js
+++ primitives/widget.js
@@ -14,6 +14,25 @@ window.TOONTALK.widget = (function () {
         dereference: function () {
             // is already dereferenced when used as part of a path
             return this;
+        },
+        copy: function () {
+            console.assert(true, "copy not implemented");
+        },
+        equals: function (other) {
+            console.assert(true, "equals not implemented");
+        },
+        update_display: function () {
+            console.assert(true, "update_display not implemented");
+        },
+        drop_on: function (other, location) {
+            console.assert(true, "drop_on not implemented");
+        },
+        to_HTML: function () {
+            console.assert(true, "to_HTML not implemented");
+        },
+        match: function (context) {
+            console.assert(true, "copy not implemented");
         }
+        
     };
 }());
