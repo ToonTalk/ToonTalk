aea1407a2679fd52e14555586b943daced2f321c
ToonTalk <toontalk@gmail.com>
2014-02-09 12:32:00 +0000

    Moved the closure compile files to its own folder

---
+++ compile/compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Ken\Documents\GitHub\ToonTalk
+rem ADVANCED_OPTIMIZATIONS caused an error
+java -jar c:\closure\compiler.jar --js actions\copy.js  --js actions\copy_constant.js --js actions\drop.js --js actions\pick_up.js --js actions\pick_up_constant.js --js primitives\widget.js --js primitives\backside.js --js primitives\condition.js --js primitives\frontside.js --js primitives\number.js --js primitives\number_backside.js --js primitives\robot.js --js support\display_updates.js --js support\for_old_browsers_only.js --js support\pick_up.js --js support\robot_actions.js --js support\run_queue.js --js support\utilities.js --js=libraries\rationaljs.js --compilation_level=SIMPLE_OPTIMIZATIONS --js_output_file=compile\toontalk.js
