1c32f88c9fa43b83c951f22af52e8e7fa11cc332
ToonTalk <toontalk@gmail.com>
2014-04-05 11:23:44 +0200

    Commented out drag id code
    No longer needed.

--- support/utilities.js
+++ support/utilities.js
@@ -241,7 +241,7 @@ window.TOONTALK.UTILITIES =
  			$element.on('dragstart', 
 			    function (event) {
 					var position = $element.get(0).getBoundingClientRect(); // $element.position();
-					var unique_id = TT.UTILITIES.generate_unique_id();
+// 					var unique_id = TT.UTILITIES.generate_unique_id();
 					var widget = $element.data("owner");
 					var is_resource = $element.is(".toontalk-top-level-resource");
 					var json_object;
@@ -256,11 +256,12 @@ window.TOONTALK.UTILITIES =
 						$element.css({width:  this.offsetWidth + "px",
 									  height: this.offsetHeight + "px"});
 					}
-					$element.attr("id", unique_id);
+					// this use of ids made obsolete by the dragee closure variable
+// 					$element.attr("id", unique_id);
 					if (event.originalEvent.dataTransfer && widget.get_json) {
 						event.originalEvent.dataTransfer.effectAllowed = is_resource ? 'copy' : 'move';
 						json_object = widget.get_json();
-						json_object.view.id_of_original_dragree = unique_id;
+// 						json_object.view.id_of_original_dragree = unique_id;
 						json_object.view.drag_x_offset = event.originalEvent.clientX - position.left;
 						json_object.view.drag_y_offset = event.originalEvent.clientY - position.top;
 						if (!json_object.width) {
@@ -315,7 +316,7 @@ window.TOONTALK.UTILITIES =
 					var json_object = TT.UTILITIES.data_transfer_json_object(event);
 					// should this set the dropEffect? https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer#dropEffect.28.29
 					var $container, container;
-                    $source = dragee || (json_object && $("#" + json_object.id_of_original_dragree));
+                    $source = dragee; // || (json_object && $("#" + json_object.id_of_original_dragree));
 					if (!$source && !json_object) {
 						if (!event.originalEvent.dataTransfer) {
 							console.log("Drop failed since there is no event.originalEvent.dataTransfer");
