3915588deca80e482e43ff5b757eb83a244d5dd1
Ken Kahn <toontalk@gmail.com>
2014-05-24 17:05:56 +0100

    Made the queue 're-entrant'
    When running the old queue is processed while the new one may be built
    up.

--- support/run_queue.js
+++ support/run_queue.js
@@ -19,9 +19,9 @@ window.TOONTALK.queue =
             return this.to_run.push(robot_context_queue);
         },
         
-        dequeue: function () {
-            return this.to_run.shift();
-        },
+//         dequeue: function () {
+//             return this.to_run.shift();
+//         },
         
         maximum_run: 1, // milliseconds
         
@@ -31,29 +31,34 @@ window.TOONTALK.queue =
             var next_robot_run, context;
             var end_time = new Date().getTime() + this.maximum_run;
             var now, element;
-//          if (this.to_run.length > 0) console.log("start time: " + (end_time-this.maximum_run));
-            while (this.to_run.length > 0) {
+            // save this array now so nothing modifies it
+            var to_run = this.to_run;
+            // clear the queue to be ready for the next test
+            this.to_run = [];
+//          if (to_run.length > 0) console.log("start time: " + (end_time-this.maximum_run));
+            while (to_run.length > 0) {
                 if (this.paused) {
-                    return;
+                    this.to_run = to_run;
+                    break;
                 }
                 now = new Date().getTime();
                 if (now >= end_time) {
+                    this.to_run = to_run;
 //                  console.log("end time:   " + now);
                     break; 
                 }
                 // TODO: use an efficient implementation of queues (linked lists?)
-                next_robot_run = this.dequeue();
+                next_robot_run = to_run.shift();
                 next_robot_run.robot.run_actions(next_robot_run.context, next_robot_run.top_level_context, next_robot_run.queue);
                 if (steps_limit) {
                     // steps_limit only used for testing
                     steps_limit -= 1;
                     if (steps_limit === 0) {
-                        // clear the queue to be ready for the next test
-                        this.to_run = [];
                         if (run_after_steps_limit) {
                             run_after_steps_limit();
                         }
-                        return;
+                        this.to_run = to_run;
+                        break;
                     }
                 }
             }
