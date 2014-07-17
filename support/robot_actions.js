efda0b07852e371fe457ba198bc60528c38da55d
Ken Kahn <toontalk@gmail.com>
2014-07-17 11:46:36 -0400

    Watched robots when hidden go home
    So when seen again they are where they started. Fixes #108.

--- support/robot_actions.js
+++ support/robot_actions.js
@@ -119,6 +119,9 @@ window.TOONTALK.actions =
                                 run_watched_step(i+1);
                             } else {
                                 // maybe user hid the robot while running
+                                // first restore robot to its 'home'
+                                frontside_element.style.left = robot_start_position.left + "px";
+                                frontside_element.style.top = robot_start_position.top + "px";
                                 for (i = i+1; i < steps.length; i++) {
                                     steps[i].run_unwatched(context, top_level_context, robot);
                                 }
@@ -148,7 +151,7 @@ window.TOONTALK.actions =
                 }
             }.bind(this);
             if (robot.get_animating()) {
-                // is animating to run a step while watched
+                // is animating so is running a step while watched
                 return true;
             }
             robot.set_animating(true);
