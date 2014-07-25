9e7a7511c42ff1aef9b11ca9cdc17ef806b8874b
Ken Kahn <toontalk@gmail.com>
2014-07-25 16:50:19 -0700

    Watched robots should be on top
    But doesn't render that way in Chrome despite higher z-index.

--- support/robot_actions.js
+++ support/robot_actions.js
@@ -116,6 +116,9 @@ window.TOONTALK.actions =
                                 // wait a bit until OK to run
                                 setTimeout(do_next_step, 500);
                             } else if (robot.visible()) {
+                                // I inspected the elements and this ensures that the robot is on top of everything
+                                // but at least in Chrome it isn't displayed that way in all situations            
+                                $(frontside_element).css({"z-index": TT.UTILITIES.next_z_index()});
                                 run_watched_step(i+1);
                             } else {
                                 // maybe user hid the robot while running
@@ -134,9 +137,6 @@ window.TOONTALK.actions =
                     if (robot.get_thing_in_hand()) {
                         robot.render();
                     }
-                    // I inspected the elements and this ensures that the robot is on top of everything
-                    // but at least in Chrome it isn't displayed that way in all situations            
-                    $(frontside_element).css({"z-index": TT.UTILITIES.next_z_index()});
                     // pause between steps and give the previous step a chance to update the DOM
                     setTimeout(do_next_step, 500);
                 };
