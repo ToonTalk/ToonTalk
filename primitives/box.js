83fe426d6ef926ab1eb14e7cc28cbd89a521f617
Ken Kahn <toontalk@gmail.com>
2014-09-01 15:07:01 +0100

    Removed obsolete code

--- primitives/box.js
+++ primitives/box.js
@@ -362,7 +362,6 @@ window.TOONTALK.box = (function (TT) {
         renderer = 
             function () {
                 var $box_hole_elements = $(frontside_element).children("." + additional_class);
-                var random_blue_color = "rgb(" + Math.round(Math.random()*128) + "," + Math.round(Math.random()*128) + ", 255)";
                 // if switching between horizontal and vertical need to remove the old elements
                 $(frontside_element).children("." + wrong_class).remove();
                 if ($box_hole_elements.length === size) {
@@ -387,8 +386,6 @@ window.TOONTALK.box = (function (TT) {
                     };
                     $box_hole_elements = $(frontside_element).children("." + additional_class);
                 }
-                // random shade of blue
-                $box_hole_elements.css({"background-color": random_blue_color});
             }.bind(this);
         if (first_time) {
             // do it now to create the elements
