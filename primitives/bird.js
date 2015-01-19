d770a7e56db6ab98d3a526bf0571e894d2eef1eb
Ken Kahn <toontalk@gmail.com>
2015-01-19 10:01:40 +0800

    Visible birds no longer deliver to hidden nests

--- primitives/bird.js
+++ primitives/bird.js
@@ -31,7 +31,8 @@ window.TOONTALK.bird = (function (TT) {
                     TT.UTILITIES.display_message("Bird can't take its nest to its nest!");
                     return false;
                 }
-                if (this.visible() || nest.visible() || nest.any_nest_copies_visible()) {
+                if (nest.visible() || nest.any_nest_copies_visible()) {
+                    // used to include this.visible() || but then when nest was on backside flew off to 0,0
                     other.save_dimensions();
                     // doesn't matter if robot is visible or there is a user event -- if either end visible show the delivery
                     frontside_element = this.get_frontside_element();
