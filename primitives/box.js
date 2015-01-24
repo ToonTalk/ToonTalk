223417c268bb06c18774835983341c5a8ea25fbd
Ken Kahn <toontalk@gmail.com>
2015-01-24 16:18:11 +0800

    Improved the display of boxes inside boxes

--- primitives/box.js
+++ primitives/box.js
@@ -316,30 +316,47 @@ window.TOONTALK.box = (function (TT) {
         var size = this.get_size();
         var update_hole = function (hole_element, hole, index) {
             var content_frontside_element = hole.get_frontside_element(true);
-            var left, top, content_frontside_element;
+            var left, top, content_frontside_element, new_width, new_height;
             if (horizontal) {
-                left = hole_width*index;
+                top = 0;
+                if ($(hole_element).parents(".toontalk-box-hole").length > 0) {
+                    new_width  = hole_width -2*border_size/size;
+                    new_height = hole_height-2*border_size;
+                } else {
+                    new_width  = hole_width;
+                    new_height = hole_height;
+                }
+                left = new_width*index;
                 if (index > 1) {
                     left += border_size*(index-1);
                 }
-                top = 0;
             } else {
                 left = 0;
-                top = hole_height*index;
+                if ($(hole_element).parents(".toontalk-box-hole").length > 0) {
+                    new_width  = hole_width -2*border_size;
+                    new_height = hole_height-2*border_size/size;
+                } else {
+                    new_width  = hole_width;
+                    new_height = hole_height;
+                }
+                top = new_height*index;
                 if (index > 1) {
                     top += border_size*(index-1);
                 }
             }
             $(hole_element).css({left:   left,
                                  top:    top,
-                                 width:  hole_width,
-                                 height: hole_height});
+                                 width:  new_width,
+                                 height: new_height});
+//             if (content_frontside_element.parentElement === hole_element) {
+//                 return;
+//             }
             if (hole_element !== content_frontside_element) {
                 // not an empty hole
                 // save dimensions first?
                 $(content_frontside_element).css({left:  0,
                                                   top:   0,
-                                                  width: '',
+                                                  width:  '',
                                                   height: ''});
                 hole_element.appendChild(content_frontside_element);
             }                                          
@@ -416,7 +433,7 @@ window.TOONTALK.box = (function (TT) {
         }
         // recompute hole dimensions taking into account border width
         if (horizontal) {
-            hole_width =  hole_width- ((size-1)*border_size)/size;
+            hole_width  = hole_width -((size-1)*border_size)/size;
         } else {
             hole_height = hole_height-((size-1)*border_size)/size;      
         }
