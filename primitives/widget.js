4fcfee107a431f46422e06f1a4abdda48741ee68
Ken Kahn <toontalk@gmail.com>
2015-02-18 14:29:12 +0800

    Removed spurious debugging warning
    Since birds now do fly off screen and back.

--- primitives/widget.js
+++ primitives/widget.js
@@ -288,9 +288,6 @@ window.TOONTALK.widget = (function (TT) {
                     if (target_absolute_position) {
                         target_absolute_position.left += left_offset;
                         target_absolute_position.top  += top_offset;
-                        if (TT.debugging && (target_absolute_position.left < 0 || target_absolute_position.top < 0)) {
-                            console.log("Should this include negative values? " + target_absolute_position);
-                        }
                     } else {
                         // can happen if a user picks up the target while this is running
                         target_absolute_position = {left: 0, top: 0};
