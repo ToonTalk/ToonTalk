022cbaa66ece812d73090e3a502abb9a2fe485d1
Ken Kahn <toontalk@gmail.com>
2016-01-13 17:59:41 +0800

    Added console log message
    Explaining that loading warnings should be ignorable.

--- compile/toontalk.js
+++ compile/toontalk.js
@@ -12,5 +12,5 @@
 var script = document.createElement("script");
 script.src = "https://toontalk.github.io/ToonTalk/toontalk.js?published=1&old_reference_to_toontalk_js=1"; 
 document.head.appendChild(script);
-
+console.log("Old published pages generate many errors but eventually should load OK and errors ignored.");
 }());
