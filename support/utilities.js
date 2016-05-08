3c71fd43f220ca58b18ed6916f5685026c8c3480
Ken Kahn <toontalk@gmail.com>
2016-05-08 11:34:11 +0100

    Published page save is immediate
    When a widget is dropped on it.

--- support/utilities.js
+++ support/utilities.js
@@ -258,7 +258,7 @@ window.TOONTALK.UTILITIES =
             $(editable_text).insertAfter(top_level_element);
             widget.set_visible(true);
             widget.render();
-            utilities.backup_all_top_level_widgets();
+            utilities.backup_all_top_level_widgets(true);
         };
         var element_under_page_x_y = function () {
             // don't include the current $source so temporarily hide it
