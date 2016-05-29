716faf3f17b674205d7e7740aa6078003061b20f
Ken Kahn <toontalk@gmail.com>
2016-05-29 18:37:50 +0100

    Fixed element rendering performance bug
    Was setting dimensions to zero and back repeatedly

--- primitives/element.js
+++ primitives/element.js
@@ -589,8 +589,12 @@ window.TOONTALK.element = (function (TT) { // TT is for convenience and more leg
             this.initialize_element();
             if (TT.UTILITIES.on_a_nest_in_a_box(frontside_element)) {
                 // need to work around a CSS problem where nested percentage widths don't behave as expected
-                this.set_attribute('width',  $(frontside_element).closest(".toontalk-box-hole").width(),  false);
-                this.set_attribute('height', $(frontside_element).closest(".toontalk-box-hole").height(), false);
+                width = $(frontside_element).closest(".toontalk-box-hole").width();
+                if (width) {
+                    this.set_attribute('width', width,  false);
+                    height = $(frontside_element).closest(".toontalk-box-hole").height();
+                    this.set_attribute('height', height, false);
+                }
             }
             if (typeof original_width === 'undefined' && frontside_element.parentElement) {
                 // if it doesn't have a parentElement it is too early
