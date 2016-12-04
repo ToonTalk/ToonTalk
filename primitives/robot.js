0bd157b30dd325c6bd46c968c0dc7c9cd108d95a
Ken Kahn <toontalk@gmail.com>
2016-12-04 15:17:35 +0000

    Unwatched robots have no training context

--- primitives/robot.js
+++ primitives/robot.js
@@ -1049,7 +1049,9 @@ window.TOONTALK.robot = (function (TT) {
     };
 
     robot.get_training_context = function () {
-        return this.get_first_in_team().get_parent_of_frontside() && this.get_first_in_team().get_parent_of_frontside().get_widget();
+        return this.visible() && 
+               this.get_first_in_team().get_parent_of_frontside() &&
+               this.get_first_in_team().get_parent_of_frontside().get_widget();
     };
     
     robot.add_step = function (step, new_widget) {
