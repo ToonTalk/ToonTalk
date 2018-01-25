99b38dba488dab9615a04da6cde768f9498bc54b
Ken Kahn <toontalk@gmail.com>
2018-01-25 17:37:27 +0700

    Improved error reporting when a robot is training robot

--- support/robot_action.js
+++ support/robot_action.js
@@ -641,8 +641,10 @@ window.TOONTALK.robot_action =
         if (robot_being_trained.get_parent_of_frontside()) {
             robot_being_trained.set_context(robot_being_trained.get_parent_of_frontside().get_widget());
         } else {
-            // seen in Sentry logs for IE11 
-            TT.UTILITIES.report_internal_error("Robot being trained by a robot that doesn't know its parent widget.");
+            // seen in Sentry logs 
+            TT.UTILITIES.report_internal_error("Robot being trained by a robot that doesn't know its parent widget. " +
+                                               " robot_being_trained is " + robot_being_trained +
+                                               " robot is " + robot);
         }
         watched_step(robot_being_trained);
     };
